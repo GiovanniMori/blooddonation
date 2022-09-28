b97801267d7a86f8d342f4fd6d1afbb521cca560
João Vitor Teles Centrone <joaovtcentrone@gmail.com>
2022-09-27 21:14:44 -0300

    Criação do modal no login e linkagem do mesmo com o login

--- src/Auth.jsx
+++ src/Auth.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import { auth } from '../firebase-config'
+// import { auth } from "../firebase-config";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 
 function Auth() {
@@ -8,31 +8,64 @@ function Auth() {
   const [registerPassword, setRegisterPassword] = useState("");
   const [loginEmail, setLoginEmail] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
+  const [open, setOpen] = useState(false);
+  const toggleModal = () => {
+    setOpen(!open);
+  };
+
   const register = async () => {
     try {
-      const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
+      const user = await createUserWithEmailAndPassword(
+        auth,
+        registerEmail,
+        registerPassword
+      );
     } catch (error) {
       console.log(error.message);
     }
   };
-  const login = async () => { };
-  const logout = async () => { };
+  const login = async () => {};
+  const logout = async () => {};
 
   return (
-    <div className="App">
+    <>
       <div>
-        <h3> Register User </h3>
-        <input type='email' placeholder="Email" onChange={(event) => {
-          setRegisterEmail(event.target.value);
-        }}
-        />
-        <input type='password' placeholder="Password" onChange={(event) => {
-          setRegisterPassword(event.target.value);
-        }}
-        />
-        <button onClick={(register)}>Criar Usuário</button>
+        <button onClick={(register) => toggleModal()}>Login</button>
       </div>
-    </div>
+
+      {open && (
+        <div className="fixed rounded-md w-[400px] ml-[-300px] mt-3 p-4 bg-white">
+          <div className="flex flex-col p-6">
+            <h1 className="text-primary font-bold text-2xl mx-auto mb-6">
+              Faça o login
+            </h1>
+            <input
+              className="p-6 hover:border-black mb-6"
+              type="email"
+              placeholder="Digite seu Email"
+              onChange={(event) => {
+                setRegisterEmail(event.target.value);
+              }}
+            />
+            <input
+              className="p-6 hover:border-black mb-6"
+              type="password"
+              placeholder="Digite sua Senha"
+              onChange={(event) => {
+                setRegisterPassword(event.target.value);
+              }}
+            />
+            <button
+              type="submit"
+              className="text-white font-bold text-xl w-[100px] mx-auto rounded-xl py-3 bg-primary"
+              onClick={() => toggleModal()}
+            >
+              Login
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import Logo from '../../dist/blood-donation.png'
+import { React, useState } from "react";
+import Logo from "../../dist/blood-donation.png";
+import Auth from "../Auth";
 
 const Navbar = () => {
+  
   return (
-    <div className='flex justify-between items-center w-full h-24 px-7 bg-primary text-white' id='navbar-container'>
-      <img src={Logo} className='w-16' />
-      <ul className="flex px-16">
-        <li className="p-4 font-bold">Home</li>
-        <li className="p-4 font-bold">Doe</li>
-        <li className="p-4 font-bold">Sobre</li>
-        <li className="p-4 font-bold">Login</li>
-      </ul>
-    </div>
-  )
-}
+    <>
+      <div
+        className="flex justify-between items-center w-full h-24 px-7 bg-primary text-white"
+        id="navbar-container"
+      >
+        <img src={Logo} className="w-16" />
+        <ul className="flex px-16">
+          <li className="p-4 font-bold">Home</li>
+          <li className="p-4 font-bold">Doe</li>
+          <li className="p-4 font-bold">Sobre</li>
+          <li className="p-4 font-bold">{<Auth />}</li>
+        </ul>
+      </div>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
