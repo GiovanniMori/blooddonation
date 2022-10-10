2a181aeaf2b3638a5dd04f47e93f3601ebbd249a
Giovanni Mori <giovanni_mori@hotmail.comn>
2022-10-10 13:11:44 -0300

    rotas e login

---
+++ src/components/LogIn.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { UserAuth } from '../contexts/AuthContext';
+import SignInGoogle from './SignInGoogle';
+
+const Signin = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+    const { signIn } = UserAuth();
+    const { user } = UserAuth();
+
+    if (user) {
+        return navigate('/')
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await signIn(email, password)
+            navigate('/account')
+        } catch (e) {
+            console.log(e.message)
+        }
+    };
+
+    return (
+        <div className='text-center max-w-[700px] mx-auto my-16 p-4'>
+            <div className="flex flex-col p-6">
+                <SignInGoogle />
+                <h1>
+                    ou
+                </h1>
+                <input
+                    className="p-6 hover:border-black mb-6 text-black border-2 border-red-500 rounded-2xl"
+                    type="email"
+                    placeholder="Digite seu Email"
+                    onChange={(event) => {
+                        setEmail(event.target.value);
+                    }}
+                ></input>
+                <input
+                    className="p-6 hover:border-black mb-6 text-black border-2 border-red-500 rounded-2xl"
+                    type="password"
+                    placeholder="Digite sua Senha"
+                    onChange={(event) => {
+                        setPassword(event.target.value);
+                    }} ></input>
+                <button
+                    type="submit"
+                    className="text-white font-bold text-xl p-2 mx-auto rounded-xl py-3 bg-primary"
+                    onClick={handleSubmit}
+                >
+                    Entrar
+                </button>
+                <p className='py-2'>
+                    Não tem uma conta ainda? {' '}
+                    <Link to='/cadastre' className='underline text-primary'>
+                        Cadastre
+                    </Link>
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Signin;
