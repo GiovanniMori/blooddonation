9400b6f7be3b7c7e26677ca11043bd273679ae44
Giovanni <giovanni_mori@hotmail.com>
2022-11-10 10:26:01 -0300

    Update error-page.jsx

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -1,16 +1,19 @@
 import { useRouteError } from "react-router-dom";
+import errorpage from "./assets/errorpage.svg";
+import Navbar from "./components/Navbar";
 
 export default function ErrorPage() {
     const error = useRouteError();
     console.error(error);
 
     return (
-        <div id="error-page">
-            <h1>Oops!</h1>
-            <p>Sorry, an unexpected error has occurred.</p>
-            <p>
-                <i>{error.statusText || error.message}</i>
-            </p>
-        </div>
+	<>
+	<Navbar/>
+        <div id="error-page" className="bg-orange-100 w-full h-auto p-32">
+            <h1 className="text-[2rem] text-slate-800 text-center"> Oops,  aconteceu um erro, a página não está disponível</h1>
+			 <img src={errorpage} className="relative ml-auto mr-auto w-[674px]"/>
+			
+		</div>
+		</>
     );
 }
