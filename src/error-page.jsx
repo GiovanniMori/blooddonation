eb932b200d081939705b48e4a500add6567bd22f
Giovanni Mori <giovanni_mori@hotmail.comn>
2022-11-12 20:22:29 -0300

    overflow navbar hidden/ responsive homepage/errorpage

--- src/error-page.jsx
+++ src/error-page.jsx
@@ -3,17 +3,23 @@ import errorpage from "./assets/errorpage.svg";
 import Navbar from "./components/Navbar";
 
 export default function ErrorPage() {
-    const error = useRouteError();
-    console.error(error);
+  const error = useRouteError();
+  console.error(error);
 
-    return (
-	<>
-	<Navbar/>
-        <div id="error-page" className="bg-orange-100 w-full h-auto p-32">
-            <h1 className="text-[2rem] text-slate-800 text-center"> Oops,  aconteceu um erro, a página não está disponível</h1>
-			 <img src={errorpage} className="relative ml-auto mr-auto w-[674px]"/>
-			
-		</div>
-		</>
-    );
+  return (
+    <>
+      <Navbar />
+      <div
+        id="error-page"
+        className="bg-orange-100  flex flex-col p-4  m-4  flex-wrap "
+      >
+        <aside className="text-[2rem] text-slate-700 text-center">
+          <h1>Oops, aconteceu um erro, a página não está disponível</h1>
+        </aside>
+        <section>
+          <img src={errorpage} className="w-auto max-w-4xl  ml-auto mr-auto " />
+        </section>
+      </div>
+    </>
+  );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -23,21 +23,23 @@ const HomePage = () => {
   return (
     <div>
       <Navbar />
-      <div className="flex justify-between pl-20 pt-10 pr-6 h-50 ">
-        <span className=" w-[525px]">
-          <h1 className="text-[3rem] mb-6">
+      <div className="flex justify-evenly p-8 pt-10 flex-wrap text-center  ">
+        <span className="w-[525px]  top-6 align-middle mt-auto mb-auto ">
+          <h1 className="text-5xl mb-6 leading-tight ">
             Você sabia que <b className="text-red-500">doar</b> sangue pode
             salvar mais de uma vida?
           </h1>
-          <h1 className="text-[2rem] mb-6">
-            Nem todo <b className="text-red-500">héroi</b> usa capa, mas todo
+          <h1 className="text-2xl mb-6">
+            Nem todo <b className="text-red-500">herói</b> usa capa, mas todo
             doador é um herói para alguém
           </h1>
-          <button className="rounded-lg w-36 h-14 bg-red-500 text-white hover:scale-105">
-            Saber mais
-          </button>
+          <aside>
+            <button className="rounded-lg w-36 h-14 bg-red-500 text-white hover:scale-105 align-middle">
+              Saber mais
+            </button>
+          </aside>
         </span>
-        <aside className=" ">
+        <aside className="max-w-5xl mt-8 ">
           <img src={Ilustration} />
         </aside>
       </div>
