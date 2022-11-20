475972b37c04ef00d096369dcdae97de2ace2cd6
João Vitor Teles Centrone <joaovtcentrone@gmail.com>
2022-11-20 18:14:05 -0300

    inicio da criação da página sobre

--- src/Caminho/Caminho.jsx
+++ src/Caminho/Caminho.jsx
@@ -1,16 +1,37 @@
 import {React, useState} from 'react'
-import Primeiro from './Primeiro'
-import Segundo from './Segundo'
-import Terceiro from './Terceiro'
-import Quarto from './Quarto'
+import Step from './Step'
 
 const Caminho = () => {
+    const t1 = "1. Sangue coletado"
+    const te1 = "É realizada a retirada do sangue por parte do doador."
+    const t2 = "2. São Realizados testes sorológicos"
+    const te2 = "O sangue doado passa por uma análise para garantir a segurança."
+    const t3 = "3. Divisão de hemocomponentes; hemácias, plaquetas e plasma"
+    const te3 = "O sangue passa por uma separação de todos os componentes."
+    const t4 = "4. O sangue é armazenado em temperatura adequada"
+    const te4 = "O sangue é encaminhado a um armazém para ficar sobre controle de temperatura."
+    const t5 = "5. Aguarda a liberação dos testes sorológicos"
+    const te5 = "Com a saída dos resultados dos testes o sangue é liberado ou não para o receptor."
+    const t6 = "6. Distribuição"
+    const te6 = "O sangue é distribuído em todos os bancos de sangue naquela região."
+    const t7 = "7. Bolsas de sangue vão para agências transfusionais"
+    const te7 = "O sangue é entregue aos pontos onde se encontraram receptores para aquele tipo."
+    const t8 = "8. Sangue chega aos pacientes para a transfusão "
+    const te8 = "O receptor realiza a transfusão do sangue."
+
     return (
-        <div className="flex absolute">
-            <Primeiro />
-            <Segundo />
-            <Terceiro />
-            <Quarto />
+        <div>
+            <h1 className="text-primary font-bold text-6xl py-10">Caminho do sangue no processo de doação</h1>
+            <div className="flex absolute">
+                <Step title={t1} text={te1}/>
+                <Step title={t2} text={te2} />
+                <Step title={t3} text={te3} />
+                <Step title={t4} text={te4} />
+                <Step title={t5} text={te5}/>
+                <Step title={t6} text={te6} />
+                <Step title={t7} text={te7} />
+                <Step title={t8} text={te8} />
+            </div>
         </div>
         
     )
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,12 +3,26 @@ import loader from "../assets/loader.json";
 import Lottie from "lottie-react";
 import Navbar from "../components/Navbar";
 import Caminho from "../Caminho/Caminho";
+import Ilustration from "../assets/ilustration.svg";
 
 const AboutDonate = () => {
   return (
     <>
       <Navbar />
-      <div className=" w-full py-8 px-10">
+      <div className="w-full py-8 px-10">
+        <div className="flex pl-20 pt-10 pr-6 h-50 justify-between">
+          <div className="flex flex-col my-auto">
+            <h1 className="text-8xl text-primary font-semibold mb-8">Voce sabia que apenas 1% da população doa sangue?</h1>
+            <h3 className="text-black text-3xl mb-8">Esse FATO é um grande problema, pois a escassez por sangue nos hemocentros nacionais
+             se torna uma realidade fazendo com que pessoas corram risco de vida por falta desse recurso natural e fundamental para nossa existência.
+           </h3>
+           <h1 className="text-red-900 inline-block text-4xl font-semibold ">O que falta para você nos ajudar nessa batalha?</h1>
+          </div>
+          <img src={Ilustration} />
+        </div> 
+        <div className="p-32">
+          <h1 className="text-7xl text-orange-900 font-bold text-center">Como saber se eu posso DOAR?</h1>
+        </div>
         <Caminho />
       </div>
     </>
