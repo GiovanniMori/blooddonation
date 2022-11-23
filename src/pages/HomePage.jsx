c6872b8c8dc3431eda86044078029af5f6f3b80b
Giovanni <giovanni_mori@hotmail.com>
2022-11-23 09:28:58 -0300

    homepage update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -27,28 +27,49 @@ const HomePage = () => {
     }, 1);
   }, []);
 
-  return (
-    <div>
-      <Navbar />
-      <div className="flex justify-between pl-20 pt-10 pr-6 h-50 ">
-        <div className="my-auto">
-        <span className=" w-[525px]">
-          <h1 className="text-[3rem] mb-6">
-            Você sabia que <b className="text-red-500">doar</b> sangue pode
-            salvar mais de uma vida?
-          </h1>
-          <h1 className="text-[2rem] mb-10">
-            Nem todo <b className="text-red-500">héroi</b> usa capa, mas todo
-            doador é um herói para alguém
-          </h1>
-          <Link to="sobre" className="rounded-lg py-7 px-7 items-center bg-red-500 text-white hover:scale-105">
-            Saber mais
-          </Link>
-        </span>
-        </div>
-        <aside className=" ">
-          <img src={Ilustration} />
-        </aside>
+   return (
+    <div className="pt-6 pl-8 pr-8 pb-6 flex justify-between text-xl text-weigh font-medium h-20 items-center overflow-hidden ">
+      <div>
+        <Link to="/" relative="path" className="flex gap-5  items-center">
+          <img src={Logo} width="30px" />
+          <h1>Blood Donation</h1>
+        </Link>
+      </div>
+      <div className=" items-center hidden lg:flex ">
+        <ul className="flex gap-20 items-center ">
+          <li>
+            <Link to="/" relative="path">
+              Início
+            </Link>
+          </li>
+          <li>
+            <Link to="/sobre" relative="path">
+              Sobre
+            </Link>
+          </li>
+          <li>
+            <Link to="/servicos" relative="path">
+              Serviços
+            </Link>
+          </li>
+          {user ? (
+            <>
+              <li className="text-red-500">{user.displayName}</li>
+              <button onClick={handleLogout}>Sair</button>
+            </>
+          ) : (
+            <>
+              <li>
+                <ModalLogIn title="Logar" />
+              </li>
+              <li className="rounded-lg p-4 bg-red-500 text-white hover:scale-105 ">
+                <Link to="/cadastrar" relative="path">
+                  Cadastrar
+                </Link>
+              </li>
+            </>
+          )}
+        </ul>
       </div>
     </div>
   );
